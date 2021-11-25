edc0be5137b8ac822fafefa82a82a4726a30bbca
Brenno Rodrigues de Assis <brennorassis@gmail.com>
2021-11-24 21:52:21 -0300

    Resolvendo desafios do projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,31 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface Image {
+  tittle:string;
+  description:string;
+  url:string;
+  ts:number;
+  id:string;
+}
+
+interface GetImagesResponse {
+  after:string;
+  data:Image[];
+}
+
 export default function Home(): JSX.Element {
+  const fetchImages = async({ pageParam = null  }):Promise<GetImagesResponse> =>{
+    const { data } = await api.get(`/api/images`, {
+      params: {
+        after:pageParam
+      }
+    });
+    return data;
+
+  }
+
+
   const {
     data,
     isLoading,
@@ -18,18 +42,29 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    fetchImages,
+    {
+      getNextPageParam: lastPage => lastPage?.after || null,
+    },
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    const formatted = data?.pages.flatMap(imageData => {
+      return imageData.data.flat();
+    });
+    console.log(formatted);
+    console.log(data?.pages);
+    return formatted;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+ if(isLoading && !isError) {
+   return <Loading />
+ }
 
-  // TODO RENDER ERROR SCREEN
+ if(isLoading && isError) {
+  return <Error />
+}
 
   return (
     <>
